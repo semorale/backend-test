68595a410796bc75f0f3fa5aa24d72387e4c2299
Sebastian Morales <sebagua@gmail.com>
2017-02-24 16:29:03 -0300

    Add forms.py and urls.py files

---
+++ django_backend_test/noras_menu/forms.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+#STDLIB importa
+
+#Core Django Imports
+from django import forms
+#Third Party apps imports
+
+#Imports local apps
+from .models import Menu, MenuItems
