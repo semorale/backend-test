7321ed72469ad4b9eaf7b1feda370472c294fa97
Sebastian Morales <sebagua@gmail.com>
2017-03-01 09:11:32 -0300

    Add ModelForm of Menu, MenuItems and Subscriber

--- django_backend_test/noras_menu/forms.py
+++ django_backend_test/noras_menu/forms.py
@@ -1,10 +1,33 @@
 # -*- encoding: utf-8 -*-
 
 #STDLIB importa
+from datetime import date
 
 #Core Django Imports
 from django import forms
+from django.forms.models import inlineformset_factory
 #Third Party apps imports
 
 #Imports local apps
-from .models import Menu, MenuItems
+from .models import Menu, MenuItems, UserSelectedLunch, Subscribers
+
+class MenuForm(forms.ModelForm):
+	day = forms.DateField(label='Menu date', input_formats=['%d-%m-%Y'])
+
+	class Meta:
+		model = Menu
+		fields = '__all__'
+
+MenuItemsFormSet = inlineformset_factory(Menu, MenuItems, fields=('name','menu',))
+
+class MenuSelectForm(forms.ModelForm):
+
+	class Meta:
+		model = UserSelectedLunch
+		fields = '__all__'
+
+class SubscribersForm(forms.ModelForm):
+
+	class Meta:
+		model = Subscribers
+		fields = '__all__'
